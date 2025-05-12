02348d9691b6eeb27ffd9e5694ad39106a13f929
Flavio-Domingues <flaviodomingues5pepsico@gmail.com>
2025-05-12 19:28:12 -0300

    user entity and resource

---
+++ src/main/java/com/cursospringboot/curso/resource/UserResourece.java
@@ -0,0 +1,19 @@
+package com.cursospringboot.curso.resource;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cursospringboot.curso.entities.User;
+
+@RestController
+@RequestMapping(value="/users")
+public class UserResourece {
+
+	@GetMapping
+	public ResponseEntity<User>findAll(){
+		User u = new User(1L, "Flavio", "flaviodomingues@gmail.com", "999999","123456" );
+		return ResponseEntity.ok().body(u);
+	}
+}
