1a6ea35deea57b28b42e8c3c54e493db40db21d8
Flavio-Domingues <flaviodomingues5pepsico@gmail.com>
2025-05-26 19:40:03 -0300

    Exception handling - update

--- src/main/java/com/cursospringboot/curso/services/UserService.java
+++ src/main/java/com/cursospringboot/curso/services/UserService.java
@@ -13,6 +13,8 @@ import com.cursospringboot.curso.repositories.UserRepository;
 import com.cursospringboot.curso.services.exceptions.DatabaseException;
 import com.cursospringboot.curso.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 
 public class UserService {
@@ -25,7 +27,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = userRepository.findById(id);
-		return obj.orElseThrow(()-> new ResourceNotFoundException(id));
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 
 	public User insert(User obj) {
@@ -35,23 +37,29 @@ public class UserService {
 	public void delete(Long id) {
 		try {
 			userRepository.deleteById(id);
-		}catch(EmptyResultDataAccessException e) {
+		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
 		}
-		
+
 	}
-	public User update(Long id , User obj) {
-		User entity = userRepository.getReferenceById(id);
-		updateData(entity, obj);
-		return userRepository.save(obj);
+
+	public User update(Long id, User obj) {
+		try {
+			User entity = userRepository.getReferenceById(id);
+			updateData(entity, obj);
+			return userRepository.save(obj);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-		
+
 	}
 }
